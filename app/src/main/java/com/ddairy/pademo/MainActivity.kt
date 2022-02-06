2dbce3239a8bb4eed02cdf283382d0a9250b8e76
DDairy <26867342+bradleytenuta@users.noreply.github.com>
2022-02-06 13:36:28 +0000

    Updated main activity file

--- app/src/main/java/com/ddairy/pademo/MainActivity.kt
+++ app/src/main/java/com/ddairy/pademo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import com.ddairy.pademo.ui.ClickCounter
@@ -16,12 +17,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PADemoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    val count by remember { mutableStateOf(0) }
+                    var count by remember { mutableStateOf(0) }
                     ClickCounter(
                         clicks = count,
-                        onClick = { count.inc() }
+                        onClick = { count = count.inc() }
                     )
                 }
             }
